772f37c00dbc30b3ae9e90d89b08b4d77e758a69
Brent Nunn <brentnunn@yahoo.com>
2015-05-02 12:01:46 -0700

    Refactored project: use array for checkerboard, etc

--- checker.py
+++ checker.py
@@ -1,167 +1,23 @@
 
-import square as sq
+import checkerboard as cb
 
 class Checker:
 
     def __init__(self, color, checkerboard):
         self.color = color
-        self.checkerboard = checkerboard
-        self.square = None
         self.king = False
-        _jump_chain = []
-        _list_of_jump_chains = []
-        _jumped_checkers = []
+        self.checkerboard = checkerboard
+        self.position = []
 
 
-        # REFACTOR: Return square, not square id
-    def get_neighboring_squares(self, square=None):
-        """ What are the possible moves from current position? """
-        if square == None:
-            square = self.square
-
+    def __str__(self):
         if self.king:
-            return(square.black_move_squares + 
-                   square.white_move_squares)
+            return ' ' + self.color[0] + 'k '
         else:
-            if self.color == 'black':
-                return self.square.black_move_squares
-            else:
-                return self.square.white_move_squares
+            return ' ' + self.color[0] + '  '
 
 
-    def get_checker(self, square_id):
-        """ Find checker associated with square_id """
-        return self.checkerboard.squares[square_id].checker
+    def set_square(self, row, column):
+            self.position = [row, column]
 
-
-    def check_for_jump(self):
-        """ Can current checker make a jump in any direction? """
-
-        for neighbors in self.get_neighboring_squares():
-            """ Check for opponent in neighboring square and that the
-                square on other side of opponent's checker is empty """
-            if (self.get_checker(neighbors[0]) and
-                self.get_checker(neighbors[0]).color != self.color and
-                self.get_checker(neighbors[1]) == None):
-
-                return True
-
-        return False
-
- 
-    def list_moves(self):
-        """ List non-jump moves from the current square.
-            Each move is a list encoded as the starting square followed
-            by the ending square. """
-
-        moves = []
-
-        # Neighboring squares without a checker are possible moves
-        for neighbors in self.get_neighboring_squares():
-            if self.get_checker(neighbors[0]) == None:
-                moves.append(self.square, neighbors[0])
-
-        return moves
-
-
-    def build_jump_chains(self, jump):
-        """ Build lists of possible jumps """
-        self._jump_chain.append(jump)
-        new_square = self.checkerboard.squares[jump[1]]
-        terminus = True
-
-        for neighbors in self.get_neighboring_squares(new_square):
-            if (neighbors[1] and
-                (self.get_checker(neighbors[1]) == None or
-                # Allow for possibly jumping in a circle back to start
-                self.get_checker(neighbors[1]) == self.checker) and
-                self.get_checker(neighbors[0]) and
-                self.get_checker(neighbors[0]).color != self.color and
-                # Prevent trying to jump the same checker twice
-                self.get_checker(neighbors[0]) not in self._jumped_checkers):
-
-                # Jumps available, we are not at the end of a jump list
-                terminus = False
-                # Keep track of checkers jumped
-                self._jumped_checkers.append(neighbors[0].checker)
-                # Recursively walk the tree of possible jumps
-                build_jump_chains(neighbors)
-
-        if terminus:
-            """ If this square did not allow any further jumps,
-                then one chain of possible jumps is complete. """
-            self._list_of_jump_chains.append(self._jump_chain)
-
-        self._jump_list.pop()
-        self._jumped_checkers.pop()
-
-        return
-
-
-    def list_jump_squares(self):
-        """ This routine returns a list of lists.
-            Each inner list, a jump chain, represents one set of jumps 
-            terminating at a square from which no further jumps are 
-            available. 
-            The first entry in the jump chain is the starting square_id.
-            Subsequent entries in the jump chain are tuples consisting
-            of the checker jumped and the square_id jumped to. """
-        
-        self._list_of_jump_chains = []
-        self._jumped_checkers = []
-
-        """ Check for jumps around starting position """
-        for neighbors in self.get_neighboring_squares():
-            if (self.get_checker(neighbors[0]) and
-                self.get_checker(neighbors[0]).color != self.color and 
-                self.neighbors[1] and
-                self.get_checker(neighbors[1]) == None):
-
-                # Starting a new jump chain
-                self._jump_chain = []
-                # Jump chains begin with the starting square_id
-                self._jump_chain.append(self.square.square_id)
-                jumped_checker = get_checker(neighbors[0])
-                self._jumped_checkers.append(jumped_checker)
-                self.build_jump_chains(neighbors)
-
-        return self._list_of_jump_chains
-
-
-    def move(self, square_id):
-        """ Move checker to a new square.
-            Break connection to old square.
-            Establish connection to new square.
-            Become a king, if appropriate. """
-
-        # Break connection to old square.
-        self.square.checker = None
-        # Establish connection to new square.
-        self.checkerboard.squares[square_id].add_checker(self)
-        #square.add_checker(self)
-        if (self.square.home_row and 
-            self.square.home_row != self.color 
-            and not self.king):
-            self.king = True
-
-
-    def jump(self, square_ids):
-        """ Jump through the list of square ids, removing jumped
-            checkers from the checkerboard. """
-
-        jump_list = square_ids
-        # Verify starting position is current checker's square
-        square_id = jump_list.pop(0)
-        if square_id != self.square.square_id:
-            # Raise error, jump list must start with current position
-            pass
-
-        for jump in jump_list:
-            self.jump_checker(jump[0])
-            self.move(jump[1])
-
-
-    def jump_checker(self, square_id):
-        """ Jump checker at specified square id """
-        self.checkerboard.remove_checker(get_checker(square_id))
 
--- checkerboard.py
+++ checkerboard.py
@@ -1,5 +1,4 @@
 
-import square as sq
 import checker as ch
 
 class Checkerboard:
@@ -8,149 +7,72 @@ class Checkerboard:
         and its squares """
 
     def __init__(self):
-        """ Create dictionary of 32 squares keyed on standard checkers 
-            notation, which numbers squares 1 through 32 """
-        self.squares = {}
-
-        self.squares[1] = sq.Square(1, 
-                                black_move_squares = ((6, 10), (5, None)),
-                                home_row = 'black')
-        self.squares[2] = sq.Square(2, 
-                                black_move_squares = ((7, 11), (6, 9)),
-                                home_row = 'black')
-        self.squares[3] = sq.Square(3,
-                                black_move_squares = ((8, 12), (7, 10)),
-                                home_row = 'black')
-        self.squares[4] = sq.Square(4,
-                                black_move_squares = ((8, 11), ),
-                                home_row = 'black')
-        self.squares[5] = sq.Square(5, 
-                                black_move_squares = ((9, 14), ),
-                                white_move_squares = ((1, None), ))
-        self.squares[6] = sq.Square(6, 
-                                black_move_squares = ((10, 15), (9, 13)),
-                                white_move_squares = ((2, None), (1, None)))
-        self.squares[7] = sq.Square(7, 
-                                black_move_squares = ((11, 16), (10, 14)),
-                                white_move_squares = ((3, None), (2, None)))
-        self.squares[8] = sq.Square(8, 
-                                black_move_squares = ((12, None), (11, 15)),
-                                white_move_squares = ((4, None), (3, None)))
-        self.squares[9] = sq.Square(9, 
-                                black_move_squares = ((14, 18), (13, None)),
-                                white_move_squares = ((6, 2), (5, None)))
-        self.squares[10] = sq.Square(10, 
-                                black_move_squares = ((15, 19), (14, 17)),
-                                white_move_squares = ((7, 3), (6, 1)))
-        self.squares[11] = sq.Square(11, 
-                                black_move_squares = ((16, 20), (15, 18)),
-                                white_move_squares = ((8, 4), (7, 2)))
-        self.squares[12] = sq.Square(12, 
-                                black_move_squares = ((16, 19), ),
-                                white_move_squares = ((8, 3), ))
-        self.squares[13] = sq.Square(13, 
-                                black_move_squares = ((17, 22), ),
-                                white_move_squares = ((9, 6), ))
-        self.squares[14] = sq.Square(14, 
-                                black_move_squares = ((18, 23), (17, 21)),
-                                white_move_squares = ((10, 7), (9, 5)))
-        self.squares[15] = sq.Square(15, 
-                                black_move_squares = ((19, 24), (18, 22)),
-                                white_move_squares = ((11, 8), (10, 6)))
-        self.squares[16] = sq.Square(16, 
-                                black_move_squares = ((20, None), (19, 23)),
-                                white_move_squares = ((12, None), (11, 7)))
-        self.squares[17] = sq.Square(17, 
-                                black_move_squares = ((22, 26), (21, None)),
-                                white_move_squares = ((14, 10), (13, None)))
-        self.squares[18] = sq.Square(18, 
-                                black_move_squares = ((23, 27), (22, 25)),
-                                white_move_squares = ((15, 11), (14, 9)))
-        self.squares[19] = sq.Square(19, 
-                                black_move_squares = ((24, 28), (23, 26)),
-                                white_move_squares = ((16, 12), (15, 10)))
-        self.squares[20] = sq.Square(20, 
-                                black_move_squares = ((24, 27), ),
-                                white_move_squares = ((16, 11), ))
-        self.squares[21] = sq.Square(21, 
-                                black_move_squares = ((25, 30), ),
-                                white_move_squares = ((17, 14), ))
-        self.squares[22] = sq.Square(22, 
-                                black_move_squares = ((26, 31), (25, 29)),
-                                white_move_squares = ((18, 15), (17, 13)))
-        self.squares[23] = sq.Square(23, 
-                                black_move_squares = ((27, 32), (26, 30)),
-                                white_move_squares = ((19, 16), (18, 14)))
-        self.squares[24] = sq.Square(24, 
-                                black_move_squares = ((29, None), (27, 31)),
-                                white_move_squares = ((20, None), (19, 15)))
-        self.squares[25] = sq.Square(25, 
-                                black_move_squares = ((30, None), (29, None)),
-                                white_move_squares = ((22, 18), (21, None)))
-        self.squares[26] = sq.Square(26, 
-                                black_move_squares = ((31, None), (30, None)),
-                                white_move_squares = ((23, 19), (22, 17)))
-        self.squares[27] = sq.Square(27, 
-                                black_move_squares = ((32, None), (31, None)),
-                                white_move_squares = ((24, 20), (23, 18)))
-        self.squares[28] = sq.Square(28, 
-                                black_move_squares = ((32, None), ),
-                                white_move_squares = ((24, 19), ))
-        self.squares[29] = sq.Square(29, 
-                                white_move_squares = ((25, 22), ),
-                                home_row = 'white')
-        self.squares[30] = sq.Square(30, 
-                                white_move_squares = ((26, 23), (25, 21)),
-                                home_row = 'white')
-        self.squares[31] = sq.Square(31, 
-                                white_move_squares = ((27, 24), (26, 22)),
-                                home_row = 'white')
-        self.squares[32] = sq.Square(32, 
-                                white_move_squares = ((28, None), (27, 23)),
-                                home_row = 'white')
+        """ Create a 2 dimensional array representing checkerboard 
+            squares """
+        self.squares = [[None for j in range(8)] for i in range(8)]
 
         self.black_checkers = []
         self.white_checkers = []
 
 
+    def print_board(self):
+        """ Print the contents of the checkerboard """
+        print()
+
+        for row in range(8):
+            for column in range(8):
+                if self.squares[row][column]:
+                    print(self.squares[row][column], end='')
+                else:
+                    if self.dark_square(row, column):
+                        print(' __ ', end='')
+                    else:
+                        print(' .  ', end='')
+            print()
+
+        print()
+
+
+    def dark_square(self, row, column):
+        """ True if this is a dark square, a valid location 
+            for a checker """
+        return (row + column) % 2 != 0
+
+
+    def place_checker(self, row, column, checker):
+        """ Place checker on square """
+        self.squares[row][column] = checker
+        checker.position = [row, column]
+
+
     def setup_new_board(self):
         """ Setup a new board with 12 checkers on each side 
-            in initial positions """
+            in starting positions """
 
         self.black_checkers = [ch.Checker('black', self) for i in range(12)]
         self.white_checkers = [ch.Checker('white', self) for i in range(12)]
 
         """ Place checkers in starting squares """
-        for enum_ch in enumerate(self.black_checkers):
-            """ Black checkers start in squares 1 through 12 """
-            self.squares[enum_ch[0] + 1].add_checker(enum_ch[1])
+        i = 0
+        for row in range(3):
+            for column in range(8):
+                if self.dark_square(row, column):
+                    self.squares[row][column] = self.white_checkers[i]
+                    i += 1
 
-        for enum_ch in enumerate(self.white_checkers):
-            """ White checkers start in squares 21 through 32 """
-            self.squares[enum_ch[0] + 21].add_checker(enum_ch[1])
-
-
-    def list_all_positions(self):
-        """ List positions of all checkers on the board """
-
-        print('Black positions')
-        for ch in self.black_checkers:
-            # Need to modify Square class to print id by default        
-            if ch.king:
-                print(ch.square.id, 'king')
-            else:
-                print(ch.square.id)
-
-        print('White positions')
-        for ch in self.white_checkers:
-            if ch.king:
-                print(ch.square.id, 'king')
-            else:
-                print(ch.square.id)
+        i = 0
+        for row in range(5, 8):
+            for column in range(8):
+                if self.dark_square(row, column):
+                    #self.squares[row][column] = self.black_checkers[i]
+                    self.place_checker(row, column, self.black_checkers[i])
+                    i += 1
 
 
+
+    """
     def remove_checker(self, checker):
-        """ Remove specified checker from play """
+        # Remove specified checker from play
         if checker.square and checker.square.checker == checker:
             checker.square.checker == None
 
@@ -159,5 +81,5 @@ class Checkerboard:
             self.black_checkers.pop(checker)
         else:
             self.white_checkers.pop(checker)
-
+    """
 
